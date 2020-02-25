f2f7602fa6e56641e76a77229dba7145d3cfc53a
C3NZ <cenz@cenz.io>
2020-02-25 12:54:26 -0800

    [fix] the header check to use the correct  to obtain information about the user

--- src/tools/validate_proxy/header_check.php
+++ src/tools/validate_proxy/header_check.php
@@ -2,14 +2,14 @@
 
 $get_headers = apache_request_headers();
 
-echo $__SERVER['REQUEST_METHOD'] . " " . 
-    $__SERVER['REQUEST_URI'] . " " . 
-    $__SERVER['SERVER_PROTOCOL'] . "<br/>";
+echo $_SERVER['REQUEST_METHOD'] . " " . 
+    $_SERVER['REQUEST_URI'] . " " . 
+    $_SERVER['SERVER_PROTOCOL'] . "<br />";
 
 foreach ($get_headers as $header => $value) {
   echo "$header: $value <br/>\n";
 }
 
-echo "<br/></br/>Your IP address is " . $__SERVER['REMOTE_ADDR'];
+echo "<br/></br/>Your IP address is " . $_SERVER['REMOTE_ADDR'];
 
 ?>
